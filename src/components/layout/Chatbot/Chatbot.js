fc99132b6db3f87885447385223e322ed49e882c
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2022-12-13 17:51:26 +0300

    continue mark up and start css of chatbot

--- src/components/layout/Chatbot/Chatbot.js
+++ src/components/layout/Chatbot/Chatbot.js
@@ -1,19 +1,26 @@
 import classes from './Chatbot.module.scss'
 import chatbot from './assets/chatbot.gif'
-import {classNames} from 'utils/helpers'
+import { classNames } from 'utils/helpers'
 // import Logo from './components/Logo/Logo.js'
-import {ReactComponent as Logo} from './assets/logo.svg'
-import {ReactComponent as Close} from './assets/close.svg'
+import { ReactComponent as Logo } from './assets/logo.svg'
+import { ReactComponent as Close } from './assets/close.svg'
+import { ReactComponent as Arrow } from './assets/arrow.svg'
 
 
-const Chatbot = ({data}) => {
+const Chatbot = ({ data }) => {
+
+  const classNamesChatbot = classNames(classes.chatbot, {
+    [classes.active]: true
+    // [classes.active]: 
+  }, [])
+
   const classNamesChat = classNames(classes.chat, {
     // [classes.open]: isChatActive
     [classes.open]: true
   }, [])
 
   return (
-    <div>
+    <div className={classNamesChatbot}>
       <div>
         <button className={classes.chatbotOpen}>
           <img src={chatbot} alt='' />
@@ -38,9 +45,19 @@ const Chatbot = ({data}) => {
                   </li>
                 )}
               </ol>
+              <span className={classes.messageDate}></span>
             </div>
           </div>
-          <footer className={classes.footer}></footer>
+          <footer className={classes.footer}>
+            <form className={classes.form}>
+              <label className={classes.input}>
+                <input type="text" placeholder="Message..." value></input>
+              </label>
+              <button>
+                <Arrow />
+              </button>
+            </form>
+          </footer>
         </div>
       </div>
     </div>
