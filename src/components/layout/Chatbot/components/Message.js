31866e7c76c1ae6cf11105399fa7cf27662e1e3a
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2022-12-27 08:13:10 +0300

    question from the list

--- src/components/layout/Chatbot/components/Message.js
+++ src/components/layout/Chatbot/components/Message.js
@@ -1,33 +1,44 @@
 import classes from './Message.module.scss'
-import { classNames } from 'utils/helpers'
+import {classNames} from 'utils/helpers'
 
-const Message = ({ message, isMessageBot, isMessageUser }) => {
+const Message = ({message, setIsQuestionAsked, setAskedQuestion, setIsLastMessageUser, askedQuestion}) => {
+  const {author, content, type} = message
   const classNamesMessage = classNames(classes.message, {
-    [classes.bot]: isMessageBot,
-    [classes.user]: isMessageUser
+    [classes.bot]: author === 'bot',
+    [classes.user]: author === 'user'
   }, [])
-  const { author, content, type } = message
-  // console.log(content)
-  if (author === 'bot' && type === 'text') {
+
+  if (type === 'text') {
     return (
       <div className={classNamesMessage}>
         {content}
       </div>
     )
   }
-
+  const handleQuestionClick = (event) => {
+    setIsQuestionAsked(true)
+    setIsLastMessageUser(true)  
+    console.log()
+    setAskedQuestion(event.target.innerHTML)  
+  }
+  let id = 0
   if (author === 'bot' && type === 'list') {
     return (
       <div className={classNamesMessage}>
         <ol>
-          {content.map((question) => {
-            console.log(question)
-            return <li key={question}>{question}</li>
+          {content.map((question) => {        
+            id++
+            return <li onClick={handleQuestionClick} className={classes.item} key={id}>
+              <span>{`${id}. `}</span>
+              <button>{question}</button>
+            </li>
           })}
         </ol>
       </div>
     )
   }
+
+
 
 }
 
