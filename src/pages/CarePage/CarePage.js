6debf7471a2a9f71a536e736dba5fbf2cc799324
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-01-16 08:03:28 +0300

    CarePage, adding in firebase

--- src/pages/CarePage/CarePage.js
+++ src/pages/CarePage/CarePage.js
@@ -1,28 +1,31 @@
 import classes from "./CarePage.module.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Text from "components/common/Text";
 import Title from "components/common/Title";
+import Image from 'components/common/Image'
+import LangContext from 'contexts/LangContext';
 
 const CarePage = () => {
   const [data, setData] = useState(null);
-  const lang = "en";
-  const classNameBody = "body";
+  const { lang } = useContext(LangContext);
 
   useEffect(() => {
-    fetch(`https://zenproject-ce905-default-rtdb.firebaseio.com/${lang}/.json`)
+    fetch(`https://zenproject-ce905-default-rtdb.firebaseio.com/${lang}/pages/care.json`)
       .then((response) => response.json())
       .then((data) => {     
-        setData(data.care);
+        setData(data);
       })
       .catch();
-  }, []);
+  }, [lang]);
+
+  console.log(data)
 
   return (
     <div>
       {data && (
-        <main>
+        <main className={classes.page}>
           <div className={classes.wrapper}>
-            <div className={classNameBody}>
+            <div className={classes.body}>
               {data.title && (
                 <Title className={classes.title} priority={data.title.priority}>
                   {data.title.content}
@@ -33,22 +36,37 @@ const CarePage = () => {
                 data.texts.map((text, index) => (
                   <Text text={text} className={classes.copy} key={`${index}`} />
                 ))}
-
-              <ul className={classes.links}>
-                {data.links?.length > 0 &&
-                  data.links.map((link, index) => (
-                    // <BrandLink link={link} key={`${index}`} />
-                    <div></div>
-                  ))}
-              </ul>
             </div>
 
-            {/* {data.image && (
-          <Image
-            className={classes.image}
-            imageData={data.image}
-          />)
-        } */}
+            {data.image && (
+              <Image className={classes.image} imageData={data.image} />
+            )}
+
+            {data.image && (
+              <Image className={classes.image} imageData={data.image} />
+            )}
+
+            <div className={classes.body}>
+     
+
+              {data.texts?.length > 0 &&
+                data.texts.map((text, index) => (
+                  <Text text={text} className={classes.copy} key={`${index}`} />
+                ))}
+            </div>
+
+            <div className={classes.body}>
+   
+
+              {data.texts?.length > 0 &&
+                data.texts.map((text, index) => (
+                  <Text text={text} className={classes.copy} key={`${index}`} />
+                ))}
+            </div>
+
+            {data.image && (
+              <Image className={classes.image} imageData={data.image} />
+            )}
 
             {/* <Preview imageData={data.image} /> */}
           </div>
