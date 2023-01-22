35ab80e3253d5b344671cc4f8f4577ab6a3895c4
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-01-22 17:31:22 +0300

    PostsPage and CarePage style

--- src/pages/PostsPage/PostsPage.js
+++ src/pages/PostsPage/PostsPage.js
@@ -11,16 +11,18 @@ const Posts = () => {
   const [dataWarranty, setDataWarranty] = useState(null);
   const [dataCare, setDataCare] = useState(null);
   const [dataCashback, setDataCashback] = useState(null);
+  const [dataClients, setDataClients] = useState(null);
 
   useEffect(() => {
-    // fetch(`${API_BASE_URL}/${lang}/.json`)
-    fetch(`https://zenproject-ce905-default-rtdb.firebaseio.com/${lang}/.json`)
+    fetch(`${API_BASE_URL}/${lang}/.json`)
+      // fetch(`https://zenproject-ce905-default-rtdb.firebaseio.com/${lang}/.json`)
       .then((response) => response.json())
       .then((data) => {
         setData(data.posts);
         setDataWarranty(data.posts.warranty);
         setDataCare(data.posts.care);
         setDataCashback(data.posts.cashback);
+        setDataClients(data.posts.clients);
       })
 
       .catch();
@@ -33,59 +35,91 @@ const Posts = () => {
           <ul className={classes.posts}>
             <li>
               <Link className={classes.post} to='/posts/warranty'>
-                <div className={classes.wrapper}>
-                  <button className={classes.image}>
-                    <img
-                      src={dataWarranty.imageSource}
-                      alt='alternate img'
-                    ></img>
+                <button className={classes.image}>
+                  <img src={dataWarranty.imageSource} alt='alternate img'></img>
+                </button>
+                <div className={classes.body}>
+                  <button className={classes.title}>
+                    {dataWarranty.title}
                   </button>
-                  <div className={classes.body}>
-                    <button className={classes.title}>
-                      {dataWarranty.title}
-                    </button>
-                    <div className={classes.article}>
-                      {dataWarranty.article.slice(0, 50)}...
-                    </div>
-                    <button className={classes.link}>Подробнее...</button>
+                  <div className={classes.article}>
+                    {dataWarranty.article.slice(0, 50)}...
                   </div>
+                  <button className={classes.link}>Read more...</button>
                 </div>
               </Link>
             </li>
             <li>
               <Link className={classes.post} to='/posts/care'>
-                <div className={classes.wrapper}>
-                  <button className={classes.image}>
-                    <img src={dataCare.imageSource} alt='alternate img'></img>
-                  </button>
-                  <div className={classes.body}>
-                    <button className={classes.title}>{dataCare.title}</button>
-                    <div className={classes.article}>
-                      {dataCare.article.slice(0, 50)}...
-                    </div>
-                    <button className={classes.link}>Подробнее...</button>
+                <button className={classes.image}>
+                  <img src={dataCare.imageSource} alt='alternate img'></img>
+                </button>
+                <div className={classes.body}>
+                  <button className={classes.title}>{dataCare.title}</button>
+                  <div className={classes.article}>
+                    {dataCare.article.slice(0, 49)}...
                   </div>
+                  <button className={classes.link}>Read more...</button>
                 </div>
               </Link>
             </li>
             <li>
               <Link className={classes.post} to='/posts/cashback'>
-                <div className={classes.wrapper}>
-                  <button className={classes.image}>
-                    <img
-                      src={dataCashback.imageSource}
-                      alt='alternate img'
-                    ></img>
+                <button className={classes.image}>
+                  <img src={dataCashback.imageSource} alt='alternate img'></img>
+                </button>
+                <div className={classes.body}>
+                  <button className={classes.title}>
+                    {dataCashback.title}
                   </button>
-                  <div className={classes.body}>
-                    <button className={classes.title}>
-                      {dataCashback.title}
-                    </button>
-                    <div className={classes.article}>
-                      {dataCashback.article.slice(0, 50)}...
-                    </div>
-                    <button className={classes.link}>Подробнее...</button>
+                  <div className={classes.article}>
+                    {dataCashback.article.slice(0, 50)}...
                   </div>
+                  <button className={classes.link}>Read more...</button>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link className={classes.post} to='/posts/clients'>
+                <button className={classes.image}>
+                  <img src={dataClients.imageSource} alt='alternate img'></img>
+                </button>
+                <div className={classes.body}>
+                  <button className={classes.title}>{dataClients.title}</button>
+                  <div className={classes.article}>
+                    {dataClients.article.slice(0, 60)}...
+                  </div>
+                  <button className={classes.link}>Read more...</button>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link className={classes.post} to='/posts/warranty'>
+                <button className={classes.image}>
+                  <img src={dataWarranty.imageSource} alt='alternate img'></img>
+                </button>
+                <div className={classes.body}>
+                  <button className={classes.title}>
+                    {dataWarranty.title}
+                  </button>
+                  <div className={classes.article}>
+                    {dataWarranty.article.slice(0, 50)}...
+                  </div>
+                  <button className={classes.link}>Read more...</button>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link className={classes.post} to='/posts/care'>
+                <button className={classes.image}>
+                  <img src={dataCare.imageSource} alt='alternate img'></img>
+                </button>
+                <div className={classes.body}>
+                  <button className={classes.title}>{dataCare.title}</button>
+                  <div className={classes.article}>
+                    {dataCare.article.slice(0,50)}...
+                  </div>
+                  <button className={classes.link}>Read more...</button>
                 </div>
               </Link>
             </li>
