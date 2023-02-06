e3b700dc3e42059776e25ade184e8fe9aa628738
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-02-06 18:00:46 +0300

    decomposition of PostsPage

--- src/pages/PostsPage/PostsPage.js
+++ src/pages/PostsPage/PostsPage.js
@@ -7,6 +7,7 @@ import { API_BASE_URL } from 'constants/api';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { setCurrentPage } from 'reducers/currentPageSlice';
+import { Item } from './components/Item';
 
 const Posts = () => {
   const [data, setData] = useState(null);
@@ -42,96 +43,14 @@ const Posts = () => {
       {data && (
         <main>
           <ul className={classes.posts}>
-            <li>
-              <Link className={classes.post} to='/posts/warranty'>
-                <button className={classes.image}>
-                  <img src={dataWarranty.imageSource} alt='alternate img'></img>
-                </button>
-                <div className={classes.body}>
-                  <button className={classes.title}>
-                    {dataWarranty.title}
-                  </button>
-                  <div className={classes.article}>
-                    {dataWarranty.article.slice(0, 50)}...
-                  </div>
-                  <button className={classes.link}>Read more...</button>
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link className={classes.post} to='/posts/care'>
-                <button className={classes.image}>
-                  <img src={dataCare.imageSource} alt='alternate img'></img>
-                </button>
-                <div className={classes.body}>
-                  <button className={classes.title}>{dataCare.title}</button>
-                  <div className={classes.article}>
-                    {dataCare.article.slice(0, 49)}...
-                  </div>
-                  <button className={classes.link}>Read more...</button>
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link className={classes.post} to='/posts/cashback'>
-                <button className={classes.image}>
-                  <img src={dataCashback.imageSource} alt='alternate img'></img>
-                </button>
-                <div className={classes.body}>
-                  <button className={classes.title}>
-                    {dataCashback.title}
-                  </button>
-                  <div className={classes.article}>
-                    {dataCashback.article.slice(0, 50)}...
-                  </div>
-                  <button className={classes.link}>Read more...</button>
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link className={classes.post} to='/posts/clients'>
-                <button className={classes.image}>
-                  <img src={dataClients.imageSource} alt='alternate img'></img>
-                </button>
-                <div className={classes.body}>
-                  <button className={classes.title}>{dataClients.title}</button>
-                  <div className={classes.article}>
-                    {dataClients.article.slice(0, 60)}...
-                  </div>
-                  <button className={classes.link}>Read more...</button>
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link className={classes.post} to='/posts/warranty'>
-                <button className={classes.image}>
-                  <img src={dataWarranty.imageSource} alt='alternate img'></img>
-                </button>
-                <div className={classes.body}>
-                  <button className={classes.title}>
-                    {dataWarranty.title}
-                  </button>
-                  <div className={classes.article}>
-                    {dataWarranty.article.slice(0, 50)}...
-                  </div>
-                  <button className={classes.link}>Read more...</button>
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link className={classes.post} to='/posts/care'>
-                <button className={classes.image}>
-                  <img src={dataCare.imageSource} alt='alternate img'></img>
-                </button>
-                <div className={classes.body}>
-                  <button className={classes.title}>{dataCare.title}</button>
-                  <div className={classes.article}>
-                    {dataCare.article.slice(0, 50)}...
-                  </div>
-                  <button className={classes.link}>Read more...</button>
-                </div>
-              </Link>
-            </li>
+            <Item data={dataCare} />
+            <Item data={dataWarranty} />
+            <Item data={dataCashback} />
+            <Item data={dataClients} />
+            <Item data={dataCare} />
+            <Item data={dataWarranty} />
+            <Item data={dataCashback} />
+            <Item data={dataClients} />
           </ul>
         </main>
       )}
