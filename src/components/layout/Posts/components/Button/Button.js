bfe0a43f97808acb4a01f80762bbf553453745ab
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-02-09 15:50:05 +0300

    Link from Button to PostsPage

--- src/components/layout/Posts/components/Button/Button.js
+++ src/components/layout/Posts/components/Button/Button.js
@@ -1,26 +1,21 @@
-import { useContext } from 'react'
+import { useContext } from 'react';
 // import OrderContext from 'contexts/ModalContext'
-import classes from './Button.module.scss'
-import { useDispatch } from 'react-redux'
-import { setIsModalActive } from 'reducers/modalSlice'
+import classes from './Button.module.scss';
+import { useDispatch } from 'react-redux';
+import { setIsModalActive } from 'reducers/modalSlice';
+import { Link } from 'react-router-dom';
 
 const Button = ({ buttonText }) => {
-  const dispatch = useDispatch()
-  // const { setIsModalActive } = useContext(OrderContext) 
-
-  const handleOrderClick = () => {
-    dispatch(setIsModalActive(true))
-  }
+  const dispatch = useDispatch();
+  // const { setIsModalActive } = useContext(OrderContext)
 
   return (
-    <button
-      onClick={handleOrderClick}
-      className={classes.button}
-      type="button"
-    >
-      {buttonText && buttonText}
-    </button>
-  )
-}
+    <Link className={classes.post} to='/posts'>
+      <button className={classes.button} type='button'>
+        {buttonText && buttonText}
+      </button>
+    </Link>
+  );
+};
 
-export default Button
+export default Button;
