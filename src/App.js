79a06f25c6e221a713e0e5d424bc21b1e36eaf45
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2023-02-10 19:43:13 +0300

    posts in Russian

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import { useLocation } from 'react-router-dom';
 
 export const App = () => {
   const { pathname } = useLocation();
+  console.log(pathname)
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
